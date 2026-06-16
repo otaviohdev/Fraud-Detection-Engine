b6771a147d0af5fe9dbaeee5750a5f00aaf834e6
devfe00 <fellipe.silva25@hotmail.com>
2026-06-16 19:44:07 -0300

    debug: log env vars na inicializacao

--- src/main/java/com/exemplo/fraudedetector/FraudeDetectorApplication.java
+++ src/main/java/com/exemplo/fraudedetector/FraudeDetectorApplication.java
@@ -6,8 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FraudeDetectorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FraudeDetectorApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        System.out.println("=== ENV CHECK ===");
+        System.out.println("FIREBASE_CREDENTIALS_JSON presente: " + 
+            (System.getenv("FIREBASE_CREDENTIALS_JSON") != null));
+        System.out.println("GEMINI_API_KEY presente: " + 
+            (System.getenv("GEMINI_API_KEY") != null));
+        System.out.println("=== FIM ENV CHECK ===");
+        SpringApplication.run(FraudeDetectorApplication.class, args);
+    }
 }
